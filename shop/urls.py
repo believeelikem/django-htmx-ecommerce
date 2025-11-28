679067f72277b46bd2ce4fd9b12d41bf58e4213d
BelieveElikem <123392549+believeelikem@users.noreply.github.com>
2025-11-28 15:51:29 +0000

    added user dashboard page

--- shop/urls.py
+++ shop/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path("", views.shop, name="dashboard"),
     path("cart/", views.cart, name="cart"),
     path("products/", views.products, name="products"),
+    path("products/detail", views.product_detail, name="product-detail"),
+    path("checkout", views.checkout, name="checkout"),
+    path("user-dashboard", views.user_dashboard, name="dashboard"),
 ]
--- shop/views.py
+++ shop/views.py
@@ -2,6 +2,8 @@ import re
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from admin_dashboard.views import dashboard
+
 def shop(request):
     return render(request, "shop/index.html")
 
@@ -10,3 +12,12 @@ def cart(request):
 
 def products(request):
     return render(request, "shop/product-listings.html")
+
+def product_detail(request):
+    return render(request, "shop/product-detail.html")
+
+def checkout(request):
+    return render(request, "shop/checkout.html")
+
+def user_dashboard(request):
+    return render(request, "shop/user-dashboard.html")
