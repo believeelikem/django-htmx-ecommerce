750724e4f0ec5891ea9a4ee199c3aa54e9eecc9b
BelieveElikem <123392549+believeelikem@users.noreply.github.com>
2025-11-28 17:02:28 +0000

    added auth templates

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.home, name="home")
+    path("sign-in/",views.sign_in, name="sign-in"),
+    path("sign-up/",views.sign_up, name="sign-up"),
+    path("profile/",views.profile, name="profile"),
+    path("password-reset/",views.password_reset, name="password-reset"),
+    path("password-update/",views.password_update, name="password-update"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,18 @@
+import re
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse("Welcome to users app")
+def sign_in(request):
+    return render(request, "users/sign-in.html")
+
+def sign_up(request):
+    return render(request, "users/sign-up.html")
+
+def profile(request):
+    return render(request, "users/profile.html")
+
+def password_reset(request):
+    return render(request, "users/password-reset.html")
+
+def password_update(request):
+    return render(request, "users/password-update.html")
