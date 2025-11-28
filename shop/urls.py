3c75baa14805da0763fc5592e25273f78e4040e4
BelieveElikem <123392549+believeelikem@users.noreply.github.com>
2025-11-28 15:08:33 +0000

    added product_listings template

--- shop/urls.py
+++ shop/urls.py
@@ -4,4 +4,6 @@ from shop import views
 
 urlpatterns = [
     path("", views.shop, name="dashboard"),
+    path("cart/", views.cart, name="cart"),
+    path("products/", views.products, name="products"),
 ]
--- shop/views.py
+++ shop/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def shop(request):
     return render(request, "shop/index.html")
+
+def cart(request):
+    return render(request, "shop/cart.html")
+
+def products(request):
+    return render(request, "shop/product_listings.html")
